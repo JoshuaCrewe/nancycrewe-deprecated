80002d002f433284ec44635e653ffdf322dd4234
Joshua Crewe <me+git@joshuacrewe.co.uk>
2017-04-08 17:07:07 +0100

    Add type scale to project

---
+++ about.php
@@ -0,0 +1,41 @@
+<?php include 'header.php' ?>
+
+<section>
+    <div class="row">
+        <div class="colum one-whole">
+
+            <div class="content">
+                
+                <p>
+                    After studying Fine Art at Falmouth University, Nancy has
+                    continued to live and work in Falmouth, Cornwall. Her
+                    preoccupation with place and the landscape is the main
+                    influence for her work, attempting to capture part of the Genis
+                    Loci of a place. Looking at the relationship between painter
+                    and place, Nancy aims to transpose this connection on to
+                    canvas.
+                </p>
+
+                <p>
+                    Working mainly from films she has collected from car windows,
+                    train journeys and walks, Nancy is able to continue to
+                    experience the landscape while watching the films back in her
+                    studio. She then approaches painting in stages, building up
+                    layers of paint. With each wash an image can be morphed into
+                    the memory of what has been seen and experienced; creating an
+                    image which alludes to the distortion of memory due to our
+                    emotional connection with particular places.
+                </p>
+
+                <p>
+                    Nancy is represented by Beside The Wave Gallery.
+                </p>
+
+            </div>
+            
+        </div>
+        
+    </div>
+</section>
+
+<?php include 'footer.php' ?>
