22b248fc9aa3a2da0f263818699be1eb3dcdc576
Joshua Crewe <me+git@joshuacrewe.co.uk>
2017-04-08 16:53:58 +0100

    Clean up gallery styles

--- paintings.php
+++ paintings.php
@@ -1,20 +1,17 @@
 <?php include 'header.php'; ?>
 
-  <section>
+<section>
     <div class="row">
 
-      <div class="gallery cf">
-          <?php for ($i = 0; $i < 11; $i++) : ?>
-          <a href="#3" class="gallery-image">
-            <div class="image" style="background-image: url('//unsplash.it/300/300')"> </div>
-            <!-- <div class="gallery&#45;image&#45;meta"> -->
-            <!--     <span class="gallery&#45;image&#45;price">£100</span> -->
-            <!--     <i class="fa fa&#45;circle sold"></i> -->
-            <!-- </div> -->
-        </a>
-        <?php endfor; ?>
+        <div class="gallery cf">
+            <?php for ($i = 0; $i < 11; $i++) : ?>
+            <a href="#3" class="gallery-item">
+                <div class="gallery-image" style="background-image: url('//unsplash.it/300/300')"> </div>
+            </a>
+            <?php endfor; ?>
+        </div>
     </div>
-  
-  </section>
+
+</section>
 
 <?php include 'footer.php'; ?>
